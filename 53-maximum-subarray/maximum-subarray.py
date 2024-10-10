2d5af1fd3e427a3ea4d7a926c253249a81a07b90
hilinaz <hiluzemedkun@gmail.com>
2024-10-10 23:12:41 +0300

    Time: 514 ms (87.41%) | Memory: 24.4 MB (96.33%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def maxSubArray(self, nums):
+        max_sum = float('-inf')
+        current_sum = 0
+        
+        for num in nums:
+            current_sum += num
+            if current_sum > max_sum:
+                max_sum = current_sum
+            if current_sum < 0:
+                current_sum = 0
+        
+        return max_sum
