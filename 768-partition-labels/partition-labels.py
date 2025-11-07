16387eaea572c9899a8b29bf0c02089f53731410
Hilina Zemedkun <hiluzemedkun@gmail.com>
2025-11-07 22:56:41 +0300

    Time: 7 ms (59.49%) | Memory: 12.4 MB (84.19%) - LeetSync

--- 768-partition-labels/partition-labels.py
+++ 768-partition-labels/partition-labels.py
@@ -1,17 +1,21 @@
 class Solution(object):
     def partitionLabels(self, s):
-        last_seen = {}
+        last_seen={}
+        answer=[]
         for i in range(len(s)):
-            last_seen[s[i]] = i
-        
-        result = []
-        start, end = 0, 0
-
+            last_seen[s[i]]=i
+        maxi =0
+        j=0
         for i in range(len(s)):
-            end = max(end, last_seen[s[i]])
+            maxi=max(last_seen[s[i]],maxi)
+            if i ==maxi:
+                answer.append(maxi-j+1)
+                j=i+1
+                maxi = 0
+        return answer
 
-            if i == end:
-                result.append(end - start + 1)
-                start = i + 1
+
+
         
-        return result
+
+      
